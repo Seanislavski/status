cb72bdd5d95074c02139b4a16234838eecef1b04
Sean Rivis <sean.rivis@gmail.com>
2017-10-26 21:12:27 -0400

    further towards the right config

--- db_update.php
+++ db_update.php
@@ -25,46 +25,60 @@ if ($connection->connect_errno) {
         echo 'mysqli_affected_rows is ' . mysqli_affected_rows($connection);
         ?><br \><?php
 
-foreach ($_POST as $a => $c) {
+foreach ($_POST as $a => $c) { //start foreach
 
     echo 'ID: ' . $i;
     ?> <br \> <?php
     echo 'Row: ' . $a;
     ?><br \><?php //here we go
     $ra = str_replace(["-", "–"], '', $a);
-
+    ?><br \><?php //here we go
+    $ftc = substr($ra, 0, 2);
+    echo 'Substring first two characters: ' . $ftc;
+    ?><br \><?php //here we go
     //Test create | Set-Vars dynamically
-    if (isset($_POST[$a])) {
-        $ra = $_POST[$a];
-        echo '$ra' . '(IF): ';
+    if (isset($_POST[$ra])) {
+        $test = $_POST[$ra];
+        echo 'test: ' . $test;
+        ?><br \><?php
+        echo '$ra' . ': ';
         echo $ra;
+        $query = "";
+        $query .=  "UPDATE $ra SET ";
+        if ($ftc == 'ET'){
+            $query .=  "ETA = $_POST[$ra]";
+        } elseif($ftc == 'NU') {
+            $query .= "NU = $_POST[$ra]";
+        } else {
+            $query .= "status" . $i . " = $_POST[$ra]";
+        }
+        $query .=  "WHERE id={$i}";
+        $result = mysqli_query($connection, $query);
+        $query = '';
     } else {
         echo $a . ' ELSE';
         $ra = $_POST[$a];
-        $ra = '';}
-//end
+        $ra = '';
+    }//end
     ?><br \><?php
-
+    print_r($result);
+    ?><br \><?php
     if (!$result) {
     die("Database query failed. " . mysqli_error($connection) . $connection->connect_error);
-    } else {
-            // $result = mysqli_query($connection, $query);
+    } else { //end if, start else
+        $result = mysqli_query($connection, $query);
         echo "Successfully Submitted Query";
         ?> <br \> <?php
-        }
-    }
+        } //end else
+    $i++;
+} //end foreach
+
     ?> <br \> <?php
-
-            ?><br \><?php
-
-
     unset($a);
     unset($b);
     unset($c);
-
  ?>
 <br \>
-
 </body>
 <?php  //release return data
     $i = 0;
